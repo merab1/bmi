d130ca833ad5ca66cfce7f3d6b51da49261ea77f
Merab <merab.tevdorashvili@gmail.com>
2022-03-21 19:20:04 +0400

    back button added on result page

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -7,8 +7,36 @@ class ResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'your BMI is' //$bmi, $advice',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'your BMI is:', //$bmi, $advice',
+              style: TextStyle(
+                color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            SizedBox(
+              height: 200,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text(
+                  'Go Back',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/button_widget.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+
+Widget ButtonWidget(String label, Function onPressed) {
+  Container(
+    width: 100,
+    decoration: BoxDecoration(
+      color: Colors.green,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: TextButton(
+      onPressed: () {
+        //Navigator.push(context, MaterialPageRoute(builder: (context) => ResultPage()));
+      },
+      child: Text(
+        'DONE',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    ),
+  ),
+}
